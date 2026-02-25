bce50ca5ca3a841b3a8452fee8e0f9131d6186c5
shivaansh27 <shivanshsharma2704@gmail.com>
2026-02-25 07:31:26 +0530

    update

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -1,9 +1,15 @@
 import admin from "firebase-admin";
 import fs from "fs";
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync("./config/serviceAccountLey.json", "utf8")
-);
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  serviceAccount = JSON.parse(
+    fs.readFileSync("./config/serviceAccountLey.json", "utf8"),
+  );
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
